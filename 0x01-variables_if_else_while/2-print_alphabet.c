4fdc68361055b76f3985348c552eb58190602ac0
Thulzin <NoksM@github.com>
2023-06-04 13:58:15 -0700

    The alphabet game

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - entry point to the programme
+ * Description: 'Assign a random number to a variable'
+ * Return: always 0(success)
+ */
+
+int main(void)
+{
+	char ch;
+
+	for
+	(ch = 'a';
+	 ch <= 'z';
+	 ch++);
+	{
+	putchar(ch);
+	putchar (' ');
+	}
+	return (0);
+}
--- 0x01-variables_if_else_while/README.md
+++ 0x01-variables_if_else_while/README.md
@@ -1,1 +1,1 @@
-The last digit
+The alphabet game
