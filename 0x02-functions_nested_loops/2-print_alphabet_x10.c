7f676ac5fcfa4cbd4426151673701f29cfc87340
Thulzin <NoksM@github.com>
2023-06-05 15:23:44 -0700

    10 x alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,10 +8,10 @@ void print_alphabet_x10(void)
 	int i;
 	int j;
 
-	for (j = 1; i <= 10; i++)
+	for (i = 1; i <= 10; i++)
 
 	{
-		for (j = 97; i <= 122; j++)
+		for (j = 97; j <= 122; j++)
 		{
 			_putchar(j);
 		}
