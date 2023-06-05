fed6a9dd9b4411e1c971453f6b7a088ff3b6c992
Thulzin <NoksM@github.com>
2023-06-05 13:48:58 -0700

    alphabet game

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * main - entry point
+ * Description: 'prints out _putchar'
+ * Return: always 0 (success)
+*/
+int main(void)
+{
+	int i;
+
+	for (i = 97; i <= 122; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
