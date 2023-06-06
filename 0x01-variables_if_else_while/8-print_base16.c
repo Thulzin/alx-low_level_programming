3595b13fb35c62ab79f5544883b6ee1541335f75
Thulzin <NoksM@github.com>
2023-06-06 11:38:10 -0700

    Hexadecimal

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -7,16 +7,15 @@
 int main(void)
 {
 	int n;
+	int m;
 
 	for (n = 48; n <= 57; n++)
 	{
 		putchar(n);
-		putchar(',');
-		putchar('');
 	}
 	for (m = 97; m <= 102; m++)
 	{
-		putchar(m)
+		putchar(m);
 	}
 	putchar ('\n');
 	return (0);
