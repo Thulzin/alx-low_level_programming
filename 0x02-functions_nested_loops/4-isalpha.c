92f51f8b01a37b2dbffdd80ffdd833275c632424
Thulzin <NoksM@github.com>
2023-06-06 13:03:06 -0700

    isalpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _isalpha - checks for alphabetic character
+ * @C: character
+ * Return: 1 if c is a letter, 0 if otherwise
+ */
+
+int _isalpha(int c)
+
+{
+	if ((c >= 97 && c <= 122) || (c >= 65 && c <= 90))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+	_putchar('\n');
+}
