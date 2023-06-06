2116f05125ac3177d148b1bc671c7605cdaaa88e
Thulzin <NoksM@github.com>
2023-06-06 13:24:22 -0700

    Sign git commit -m Sign

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,9 +1,9 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
  * _isalpha - checks for alphabetic character
- * @C: character
+ * Parameter:
+ * -C: 'character in ASCII table'
  * Return: 1 if c is a letter, 0 if otherwise
  */
 
