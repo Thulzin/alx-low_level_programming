1c0dc23d7ee8b8b03a45967f14f5933ab64a0c0f
Thulzin <Thulzin@github.com>
2023-12-23 15:03:02 -0800

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
-/*
- *main - a C program that prints exactly
- *Return: 0(Successs)
+
+/**
+ *main - Entry point
+ *Description:  a C program that prints exactly this
+ *Return: 0(Success)
  */
 
 int main(void)
 
 {
 	puts("Programming is like building a multilingual puzzle\n");
-	return 0;
+	return (0);
 }
