ce24e17fe5d1eae2432cc09b23aab0a6e40a286c
Thulzin <Thulzin@github.com>
2024-01-02 16:42:54 -0800

    I fear lack of computers

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ *_strlen - a function that returns the length of a string
+ *@s: function parameter
+ *Return: length of string
+ */
+
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+
+	return (0);
+}
