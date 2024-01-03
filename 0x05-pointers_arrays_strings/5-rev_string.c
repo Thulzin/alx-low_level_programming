8bd48a7444610c092379e8d615ddd4a15730910a
Thulzin <Thulzin@github.com>
2024-01-02 17:04:46 -0800

    Reversing a string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * rev_string - a function that reverses a string
+ * @s: function parameter
+ */
+
+void rev_string(char *s)
+{
+	int i;
+	int str = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+		str++;
+	for (i = 0; i < str / 2; i++)
+	{
+		char j
+			j = s[i];
+		s[i] = s[str - 1 - i];
+		s[str - 1 - i] = j;
+	}
+}
