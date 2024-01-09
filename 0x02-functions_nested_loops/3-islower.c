a4dc1e42a5473e5c363ec8f803678c65517229e3
Thulzin <Thulzin@github.com>
2024-01-09 13:51:10 -0800

    islower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _islower -  a function that checks for lowercase character
+ * @c: Character to be checked
+ * Return: 1 for lowercase and 0 for anything else
+ */
+
+int _islower(int c)
+{
+	if (c >= 97 && c <= 122)
+	{
+		return (0);
+	}
+	return (0);
+}
