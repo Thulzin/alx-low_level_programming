9013113219ec01e106a49cb44a318620cd869e5d
Thulzin <Thulzin@github.com>
2024-01-09 12:01:40 -0800

    Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - a program that prints all single digit numbers of base 10
+ * Return: 0
+ */
+
+int main(void)
+{
+	int num;
+
+	while (num < 10)
+	{
+		printf("%d", num);
+		num += 1;
+	}
+	printf("\n");
+	return (0);
+}
