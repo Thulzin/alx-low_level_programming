62e9d8c97bfdbf50adc59ced0e3bc52b400e8094
Thulzin <Thulzin@github.com>
2024-01-09 18:13:58 -0800

    isupper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _isupper - a function that checks for uppercase character
+ * @c: character
+ * Return: 1 if c is uppercase or 0 if otherwise
+ */
+
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
