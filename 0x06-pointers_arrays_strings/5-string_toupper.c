e464da261045986ff925e3a334b0e2ab533c8b41
Thulzin <Thulzin@github.com>
2024-04-11 11:57:03 -0700

    lowercases to uppercases

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * string_toupper - a function that changes all lowercase
+ * letters of a string to uppercase
+ * @n: all letters
+ * Return: 0 always
+ */
+
+char *string_toupper(char *n)
+{
+	int i;
+
+	i = 0;
+	while (n[i] != '\0')
+	{
+		if (n[i] >= 'a' && n[i] <= 'z')
+		{
+			n[i] = n[i] - 32;
+		}
+		i++;
+	}
+
+	return (n);
+}
