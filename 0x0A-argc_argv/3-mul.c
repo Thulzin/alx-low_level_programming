5345dd5b3a85e86e1fb7f9dabbca0c258143ce4d
Thulzin <Thulzin@github.com>
2024-06-25 13:49:37 -0700

    a program that multiplies two numbers

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -12,9 +12,9 @@
 int main(int argc, char *argv[])
 {
 	int num1 = atoi(argv[1]);
-        int num2 = atoi(argv[2]);
+	int num2 = atoi(argv[2]);
 
-        int result = num1 * num2;
+	int result = num1 * num2;
 
 	if (argc != 3)
 	{
